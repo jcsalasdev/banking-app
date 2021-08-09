7dff760bb71317f295e6b5e64056ffcda784fe0b
jcsalasdev <jcsalasdev@gmail.com>
2021-08-09 19:58:30 +0800

    budget 65%

--- src/App.js
+++ src/App.js
@@ -1,44 +1,40 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import NewExpense from './components/NewExpense/NewExpense';
-import Expenses from './components/Expenses/Expenses';
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses/Expenses";
 
 const App = () => {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450.01,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+	const [expenses, setExpenses] = useState([
+		{
+			id: "e1",
+			title: "Paper",
+			amount: 9400.12,
+			date: new Date(2020, 7, 14),
+		},
+		{
+			id: "e1",
+			title: "Toilet Paper",
+			amount: 11924.12,
+			date: new Date(2020, 7, 14),
+		},
+		{
+			id: "e2",
+			title: "New TV",
+			amount: 799.49,
+			date: new Date(2021, 2, 12),
+		},
+	]);
 
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement('h2', {}, "Let's get started!"),
-  //   React.createElement(Expenses, { items: expenses })
-  // );
+	const addExpenseHandler = (expense) => {
+		setExpenses([...expenses, expense]);
+	};
 
-  return (
-    <div>
-      <NewExpense />
-      <Expenses items={expenses} />
-    </div>
-  );
-}
+	return (
+		<div>
+			<NewExpense onAddExpense={addExpenseHandler} />
+			<Expenses items={expenses} />
+		</div>
+	);
+};
 
 export default App;
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,31 @@
-import React, { useState } from 'react';
+import React from "react";
 
-import ExpenseDate from './ExpenseDate';
-import Card from '../UI/Card';
-import './ExpenseItem.css';
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const clickHandler = () => {
-    console.log("deleted");
-  };
-
-  return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-      </div>
-      <button className='expense-item__btn'onClick={clickHandler}>X</button>
-    </Card>
-  );
-}
+	const clickHandler = () => {
+		console.log("deleted");
+	};
+	return (
+		<Card className="expense-item">
+			<ExpenseDate date={props.date} />
+			<div className="expense-item__description">
+				<h2>{props.title}</h2>
+				<div className="expense-item__price">
+					{Number(props.amount).toLocaleString("en-US", {
+						style: "currency",
+						currency: "USD",
+					})}
+				</div>
+			</div>
+			<button
+				className="expense-item__btn far fa-trash-alt"
+				onClick={clickHandler}
+			/>
+		</Card>
+	);
+};
 
 export default ExpenseItem;
